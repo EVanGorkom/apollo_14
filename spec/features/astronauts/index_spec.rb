d5c59d283d4e4153ebb2e8b22a9462d3441c9c7a
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-24 21:16:15 -0500

    Passed the first 4 tests of the IC

---
+++ spec/features/astronauts/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe "Astronaut Index Page" do
+  before :each do
+    @astro1 = Astronaut.create!(name: "Buzz Aldren", age: 35, job: "Pilot")
+    @astro2 = Astronaut.create!(name: "Neil Armstrong", age: 36, job: "Pilot")
+    @astro3 = Astronaut.create!(name: "Michael Collins", age: 37, job: "Mechanic")
+  end
+
+  describe "Astronaut Details" do
+    xit "Shows the detailed information of each astronaut on the page" do
+      expect(page).to have_content("Name: #{@astro1.name}, Age: #{@astro1.age}, Job: #{@astro1.job}")
+      expect(page).to have_content("Name: #{@astro2.name}, Age: #{@astro2.age}, Job: #{@astro2.job}")
+      expect(page).to have_content("Name: #{@astro3.name}, Age: #{@astro3.age}, Job: #{@astro3.job}")
+    end
+  end
+end
