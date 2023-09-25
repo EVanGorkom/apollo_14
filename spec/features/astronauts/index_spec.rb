d7593a622af8a924c53168398d5f5f28e8dd360b
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-09-24 21:31:52 -0500

    Finish US1

--- spec/features/astronauts/index_spec.rb
+++ spec/features/astronauts/index_spec.rb
@@ -5,10 +5,13 @@ RSpec.describe "Astronaut Index Page" do
     @astro1 = Astronaut.create!(name: "Buzz Aldren", age: 35, job: "Pilot")
     @astro2 = Astronaut.create!(name: "Neil Armstrong", age: 36, job: "Pilot")
     @astro3 = Astronaut.create!(name: "Michael Collins", age: 37, job: "Mechanic")
+
+    visit "/astronauts"
   end
 
   describe "Astronaut Details" do
-    xit "Shows the detailed information of each astronaut on the page" do
+    it "Shows the detailed information of each astronaut on the page" do
+      save_and_open_page
       expect(page).to have_content("Name: #{@astro1.name}, Age: #{@astro1.age}, Job: #{@astro1.job}")
       expect(page).to have_content("Name: #{@astro2.name}, Age: #{@astro2.age}, Job: #{@astro2.job}")
       expect(page).to have_content("Name: #{@astro3.name}, Age: #{@astro3.age}, Job: #{@astro3.job}")
